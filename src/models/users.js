1757bff0665ba3d6bc4f408892b432bea78563fb
tyassusanto <tyassusanto>
2021-12-19 23:27:55 +0700

    model users

---
+++ src/models/users.js
@@ -0,0 +1,81 @@
+const connection = require('../config/connection')
+
+const insertUsers = (inserDataUsers) => {
+    return new Promise((resolve, reject) => {
+        connection.query("INSERT INTO users set ?", inserDataUsers, (error, result)=>{
+            if(!error){
+                resolve(result)
+            }else{  
+                reject(error)
+            }
+        })
+    })
+}
+
+const getAllUsers = () => {
+    return new Promise((resolve, reject) => {
+        connection.query("SELECT * FROM users",(error, result)=>{
+        if(!error){
+            resolve(result)
+        }else{
+            reject(error)
+        }
+        })
+    })
+}
+
+const updateUsers = (update, id) => {
+    return new Promise((resolve, reject) => {
+        connection.query("UPDATE users SET ? WHERE id = ?", [update, id], (error, result) => {
+            if(!error){
+                resolve(result)
+            }else{
+                reject(error)
+            }
+        })
+    })
+}
+
+const deleteUsers = (id) => {
+    return new Promise((resolve, reject) => {
+        connection.query("DELETE FROM users WHERE id = ?", id, (error, result) => {
+            if(!error){
+                resolve(result)
+            }else{
+                reject(error)
+            }
+        })
+    })
+}
+// TAMBAHKAN DENGAN JOIN
+const detailUsers = (id) => {
+    return new Promise((resolve, reject) => {
+        connection.query("SELECT * FROM users WHERE id = ?", id, (error, result) => {
+            if(!error){
+                resolve(result)
+            }else{
+                reject(error)
+            }
+        })
+    })
+}
+const detailUserss = (name) => {
+    return new Promise((resolve, reject) => {
+        connection.query("SELECT * FROM users WHERE name LIKE ?", name, (error, result) => {
+            if(!error){
+                resolve(result)
+            }else{
+                reject(error)
+            }
+        })
+    })
+}
+
+module.exports = {
+    insertUsers,
+    getAllUsers,
+    updateUsers,
+    deleteUsers,
+    detailUsers,
+    detailUserss
+}
