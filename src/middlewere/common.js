767618c8096894e8c6f602070b42cd1549c79877
tyassusanto <tyassusanto>
2021-12-27 02:55:35 +0700

    add middlewere

--- src/middlewere/common.js
+++ src/middlewere/common.js
@@ -1,7 +1,27 @@
+const joi = require('joi')
+const createError = require('http-errors')
+
 const myConsole = (req, res, next)=>{ 
     console.log('middlewere');
     next()
 }
+
+// middlewere validation update users
+const validationUserUpdate = (req, res, next) => {
+    const {name, phone} = req.body
+    const schema = joi.object({
+        name : joi.string().max(128).required(),
+        phone : joi.string().max(16).regex(/[0-9]/)
+    })
+    const {error} = schema.validate(req.body)
+    if(error){
+        const errorMessage = error.details[0].message.toString()
+        return next(createError(422, errorMessage))
+    }
+    next()
+}
+
 module.exports = {
-    myConsole
+    myConsole,
+    validationUserUpdate
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const route = express.Router()
 const usersController = require('../controller/users')
+const commonMiddlewere = require('../middlewere/common')
 
 route.post('/register', usersController.register)
 route.post('/login', usersController.login)
 route.get('/search', usersController.findAllUsers)
-route.put('/update/:id', usersController.updateUsers)
+route.put('/update/:id', commonMiddlewere.validationUserUpdate, usersController.updateUsers)
 route.get('/profile/:id', usersController.detailUsers)
 
 module.exports = route
