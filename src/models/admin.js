964bd51efc8933c67eead7d5e6c0d34fd14bad7b
tyassusanto <tyassusanto>
2021-12-27 10:00:15 +0700

    minor changes

--- src/models/admin.js
+++ src/models/admin.js
@@ -2,7 +2,7 @@ const connection = require('../config/connection')
 
 const getUsers = () => {
     return new Promise((resolve, reject) => {
-        connection.query("SELECT id, username, name, balance.wallet FROM users RIGHT JOIN wallet on users.id = wallet.user_id", (error, result) => {
+        connection.query("SELECT id, username, name, balance FROM users LEFT JOIN wallet on users.id = wallet.user_id", (error, result) => {
             if(!error){
                 resolve(result)
             }else{
